ebad25576f20abc885e29a1363d27413852d91de
krkarma777 <krkarma777@gmail.com>
2024-05-11 16:18:59 +0900

    Refactor User entity to use constructor with DTO
    Refactored the User entity within the com.leaf.domain.entities package to utilize a constructor that accepts a UserCreateRequestDTO. This change streamlines the process of creating a User entity by directly mapping fields from the DTO.
    
    - Added a new constructor to the User entity that accepts a UserCreateRequestDTO as a parameter.
    - Implemented logic in the constructor to map email and password fields from the DTO to the corresponding entity fields.
    
    This modification improves code readability and maintains separation of concerns by centralizing the user creation logic within the User entity.

--- src/main/java/com/leaf/domain/entities/User.java
+++ src/main/java/com/leaf/domain/entities/User.java
@@ -1,8 +1,15 @@
 package com.leaf.domain.entities;
 
+import com.leaf.domain.dtos.UserCreateRequestDTO;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
 public class User {
 
     @Id
@@ -14,4 +21,9 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    public User(UserCreateRequestDTO requestDTO) {
+        this.email = requestDTO.getEmail();
+        this.password = requestDTO.getPassword();
+    }
 }
