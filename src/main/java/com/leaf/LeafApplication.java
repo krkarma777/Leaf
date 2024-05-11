d99e1c2994f4c13a348bd823f94b8ef8e0d2f621
krkarma777 <krkarma777@gmail.com>
2024-05-11 14:40:35 +0900

    Initial commit

---
+++ src/main/java/com/leaf/LeafApplication.java
@@ -0,0 +1,13 @@
+package com.leaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeafApplication.class, args);
+    }
+
+}
