ab068e0fa94dfed57f83e99492b6d6cab8f451e1
krkarma777 <krkarma777@gmail.com>
2024-05-11 16:08:46 +0900

    Add UserRepository and UserService
    Created UserRepository interface and UserService class to manage user data access and business logic, respectively.
    
    1. UserRepository:
       - Added a new interface named UserRepository within the `com.leaf.domain.repositories` package.
       - Extends JpaRepository<User, Long> for CRUD operations on User entities.
       - Annotated with @Repository to indicate it as a Spring repository.
    
    2. UserService:
       - Created a new class named UserService within the `com.leaf.domain.services` package.
       - Annotated with @Service to mark it as a Spring service component.
       - Utilizes constructor injection (@RequiredArgsConstructor) to inject the UserRepository dependency.
    
    These additions lay the foundation for managing user data and implementing business logic related to users in the application.

---
+++ src/main/java/com/leaf/domain/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.leaf.domain.repositories;
+
+import com.leaf.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ src/main/java/com/leaf/domain/services/UserService.java
@@ -0,0 +1,11 @@
+package com.leaf.domain.services;
+
+import com.leaf.domain.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+}
