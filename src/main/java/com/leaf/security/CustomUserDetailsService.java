478d6be569e7d18ca129b28ee06929b78fab4d21
krkarma777 <krkarma777@gmail.com>
2024-05-12 15:00:03 +0900

    Add CustomUserDetailsService class
    - Implemented CustomUserDetailsService class to load user details by username (email)
    - Utilizes UserRepository to retrieve user by email
    - Throws UsernameNotFoundException if user with the specified email is not found

---
+++ src/main/java/com/leaf/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.leaf.security;
+
+import com.leaf.domain.entities.User;
+import com.leaf.domain.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new CustomUserDetails(user);
+    }
+}
