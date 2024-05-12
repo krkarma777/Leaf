1756d8198f269b4b8b824a9824934c6de40351e6
krkarma777 <krkarma777@gmail.com>
2024-05-12 14:59:46 +0900

    Add CustomUserDetails class
    - Created CustomUserDetails class implementing UserDetails interface to provide custom user details for Spring Security authentication
    - Retrieves user authorities based on the user's role
    - Overrides methods for password, username, and account status

---
+++ src/main/java/com/leaf/security/CustomUserDetails.java
@@ -0,0 +1,66 @@
+package com.leaf.security;
+
+
+import com.leaf.domain.entities.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+
+        collection.add(new GrantedAuthority() {
+
+            @Override
+            public String getAuthority() {
+                return user.getRole().toString();
+            }
+        });
+
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+
+        return user.getEmail();
+    }
+
+    // 계정의 만료
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
