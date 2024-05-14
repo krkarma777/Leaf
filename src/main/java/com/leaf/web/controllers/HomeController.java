718e5daabe0918afe74e04d9cea6cf39f43ea04a
krkarma777 <krkarma777@gmail.com>
2024-05-14 10:18:15 +0900

    feat: Add register endpoint to HomeController
    Added a new endpoint for handling GET requests to "/register" in the HomeController class. This endpoint returns the "register" view, allowing users to access the registration page.

--- src/main/java/com/leaf/web/controllers/HomeController.java
+++ src/main/java/com/leaf/web/controllers/HomeController.java
@@ -15,4 +15,9 @@ public class HomeController {
     public String login() {
         return "login";
     }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
 }
