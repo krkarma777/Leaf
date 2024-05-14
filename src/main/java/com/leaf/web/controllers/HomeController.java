66dd33c7916f06bdb3b2611356f03fc5841e1ebb
krkarma777 <krkarma777@gmail.com>
2024-05-14 10:12:32 +0900

    feat: Add login endpoint to HomeController
    Added a new endpoint for handling GET requests to "/login" in the HomeController class. This endpoint returns the "login" view, allowing users to access the login page.

--- src/main/java/com/leaf/web/controllers/HomeController.java
+++ src/main/java/com/leaf/web/controllers/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home() {
         return "home";
     }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 }
