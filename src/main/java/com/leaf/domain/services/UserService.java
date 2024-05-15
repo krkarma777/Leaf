6e50ac488491234ced947d29a5d9719e841d2d66
krkarma777 <krkarma777@gmail.com>
2024-05-15 11:24:14 +0900

    feat: Add method to find user by email in UserService
    - Added findByEmail method to UserService
    - Throws ResponseStatusException if user is not found
    - Enhanced error handling for user lookup

--- src/main/java/com/leaf/domain/services/UserService.java
+++ src/main/java/com/leaf/domain/services/UserService.java
@@ -4,8 +4,10 @@ import com.leaf.domain.dtos.UserCreateRequestDTO;
 import com.leaf.domain.entities.User;
 import com.leaf.domain.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -19,5 +21,10 @@ public class UserService {
         requestDTO.setPassword(encodedPW);
         User user = new User(requestDTO);
         return userRepository.save(user);
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "user not found"));
     }
 }
