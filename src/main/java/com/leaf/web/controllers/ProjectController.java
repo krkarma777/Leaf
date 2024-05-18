906ab9f673a8307e33301f07dd503ffe108cc33d
krkarma777 <krkarma777@gmail.com>
2024-05-18 14:32:20 +0900

    feat: Add createProject endpoint to ProjectController
    - Added a POST endpoint to ProjectController for creating projects.
    - Integrated the createProject method to handle ProjectCreateRequestDTO and Principal.
    - Utilized ProjectService to save the new project and return the response.
    
    This enhancement allows the creation of new projects through a dedicated API endpoint, leveraging the ProjectService for business logic.

--- src/main/java/com/leaf/web/controllers/ProjectController.java
+++ src/main/java/com/leaf/web/controllers/ProjectController.java
@@ -1,9 +1,15 @@
 package com.leaf.web.controllers;
 
+import com.leaf.domain.dtos.project.ProjectCreateRequestDTO;
 import com.leaf.domain.services.ProjectService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProjectController {
 
     private final ProjectService projectService;
+
+    @PostMapping
+    public ResponseEntity<?> createProject(@RequestBody ProjectCreateRequestDTO requestDTO, Principal principal) {
+        return ResponseEntity.ok(projectService.save(requestDTO, principal));
+    }
 }
