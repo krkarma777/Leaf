282f5de8d76d6c88ad8043b446fa8dbcde154547
krkarma777 <krkarma777@gmail.com>
2024-05-18 14:31:17 +0900

    feat: Enhance ProjectService with save methods for Project and ProjectCreateRequestDTO
    - Added a save method to ProjectService to persist Project entities.
    - Implemented an overloaded save method that creates and saves a Project from ProjectCreateRequestDTO and Principal.
    - Incorporated userService to resolve project manager and team member details.
    
    These changes improve the ProjectService by providing flexible save operations and simplifying project creation from DTOs.

--- src/main/java/com/leaf/domain/services/ProjectService.java
+++ src/main/java/com/leaf/domain/services/ProjectService.java
@@ -1,12 +1,33 @@
 package com.leaf.domain.services;
 
+import com.leaf.domain.dtos.project.ProjectCreateRequestDTO;
+import com.leaf.domain.entities.Project;
+import com.leaf.domain.entities.User;
 import com.leaf.domain.repositories.ProjectRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.security.Principal;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class ProjectService {
 
     private final ProjectRepository projectRepository;
+    private final UserService userService;
+
+    public Project save(Project project) {
+        return projectRepository.save(project);
+    }
+
+    public Project save(ProjectCreateRequestDTO requestDTO, Principal principal) {
+        User projectManager = userService.findByEmail(principal.getName());
+
+        Set<User> teamMembers = requestDTO.getTeamMemberIds().stream().map(userService::findById).collect(Collectors.toSet());
+        teamMembers.add(projectManager);
+
+        return projectRepository.save(new Project(requestDTO, projectManager, teamMembers));
+    }
 }
