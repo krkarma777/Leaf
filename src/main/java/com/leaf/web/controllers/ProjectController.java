1e17ffc0bf134ecc628bcf85618c4231373097a1
krkarma777 <krkarma777@gmail.com>
2024-05-18 15:43:20 +0900

    refactor: Use ValidationErrorHandler utility in ProjectController
    - Refactored ProjectController to utilize the ValidationErrorHandler utility for handling validation errors.
    - Removed inline error handling logic and replaced it with a call to ValidationErrorHandler.handleValidationErrors.
    - Ensured cleaner and more maintainable code by separating validation concerns.

--- src/main/java/com/leaf/web/controllers/ProjectController.java
+++ src/main/java/com/leaf/web/controllers/ProjectController.java
@@ -2,11 +2,10 @@ package com.leaf.web.controllers;
 
 import com.leaf.domain.dtos.project.ProjectCreateRequestDTO;
 import com.leaf.domain.services.ProjectService;
+import com.leaf.web.utils.ValidationErrorHandler;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,8 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,17 +24,9 @@ public class ProjectController {
     @PostMapping
     public ResponseEntity<?> createProject(@RequestBody @Validated ProjectCreateRequestDTO requestDTO, Principal principal, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            Map<String, String> errors = getErrors(bindingResult);
-            String s = errors.values().stream().toList().get(0);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(Map.of("message", s, "errors", errors));
+            return ValidationErrorHandler.handleValidationErrors(bindingResult);
         }
 
         return ResponseEntity.ok(projectService.save(requestDTO, principal));
-    }
-
-    private Map<String, String> getErrors(BindingResult bindingResult) {
-        return bindingResult.getFieldErrors().stream()
-                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
     }
 }
