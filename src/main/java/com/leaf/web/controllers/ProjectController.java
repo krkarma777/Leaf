1885bb3a2f159f71a4e8a491ee32bafd50eec89d
krkarma777 <krkarma777@gmail.com>
2024-05-23 12:24:07 +0900

    Add endpoint to retrieve projects associated with the logged-in user
    This commit adds a new endpoint to the ProjectController class, allowing users to retrieve projects associated with their account. The getProjectsByUserEmail method utilizes the ProjectService's findByPrincipal method to fetch projects based on the logged-in user's email. The endpoint supports pagination and returns projects sorted by start date in descending order by default.

--- src/main/java/com/leaf/web/controllers/ProjectController.java
+++ src/main/java/com/leaf/web/controllers/ProjectController.java
@@ -1,16 +1,18 @@
 package com.leaf.web.controllers;
 
 import com.leaf.domain.dtos.project.ProjectCreateRequestDTO;
+import com.leaf.domain.entities.Project;
 import com.leaf.domain.services.ProjectService;
 import com.leaf.web.utils.ValidationErrorHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -28,5 +30,13 @@ public class ProjectController {
         }
 
         return ResponseEntity.ok(projectService.save(requestDTO, principal));
+    }
+
+    @GetMapping("/my-projects")
+    public ResponseEntity<Page<Project>> getProjectsByUserEmail(
+            Principal principal,
+            @PageableDefault(sort = "startDate", direction = Sort.Direction.DESC) Pageable pageable
+    ) {
+        return ResponseEntity.ok(projectService.findByPrincipal(principal, pageable));
     }
 }
