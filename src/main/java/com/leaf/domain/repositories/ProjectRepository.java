a9b2b6a3a7b6f49f32bf099c4a9909b00b0f1e90
krkarma777 <krkarma777@gmail.com>
2024-05-23 12:22:59 +0900

    Add query to retrieve projects associated with a user
    This commit adds a custom query in the ProjectRepository interface to fetch projects based on the user's association with them. The query selects projects where the user is either a team member or the project manager. This enhances the repository's functionality by enabling more specific project retrieval based on user relationships.

--- src/main/java/com/leaf/domain/repositories/ProjectRepository.java
+++ src/main/java/com/leaf/domain/repositories/ProjectRepository.java
@@ -1,9 +1,16 @@
 package com.leaf.domain.repositories;
 
 import com.leaf.domain.entities.Project;
+import com.leaf.domain.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    @Query("SELECT p FROM Project p JOIN p.teamMembers tm WHERE tm = :user OR p.projectManager = :user")
+    Page<Project> findProjectsByUser(@Param("user") User user, Pageable pageable);
 }
