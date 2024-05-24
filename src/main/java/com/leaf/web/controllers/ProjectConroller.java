48ee1e5c955bd63f9032b6da0ba37426d93efd22
krkarma777 <krkarma777@gmail.com>
2024-05-24 23:54:36 +0900

    Added endpoint for editing projects

--- src/main/java/com/leaf/web/controllers/ProjectConroller.java
+++ src/main/java/com/leaf/web/controllers/ProjectConroller.java
@@ -3,6 +3,7 @@ package com.leaf.web.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
@@ -22,5 +23,12 @@ public class ProjectConroller {
         String currentDateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm"));
         model.addAttribute("currentDateTime", currentDateTime);
         return "project/create-project";
+    }
+
+    @GetMapping("/edit")
+    public String editProjectForm(Model model) {
+        String currentDateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm"));
+        model.addAttribute("currentDateTime", currentDateTime);
+        return "project/edit-project";
     }
 }
